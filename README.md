8083506872f2545d6dea9cb4c1c769ff4f5c5740
prosperjohn9 <prosperjohn9@gmail.com>
2023-09-19 01:26:24 +0300

    added_github_profile_link

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# etch-a-sketch
+# Etch A Sketch
 Etch-a-sketch project created using html,css and javascript. 
+
+
+Live Demo:
+https://prosperjohn9.github.io/etch-a-sketch/
+
+
+Desktop View:
+
+
+![Desktop View](./images/home.png)
+
+
+This project was created for the [Etch-A-Sketch](https://www.theodinproject.com/lessons/foundations-etch-a-sketch) assignment as part of The Odin Project curriculum.
+
+
+# Objectives
+1. Website with 16x16 grid of square <div> elements.
+2. Change color of each <div> on mouse-over.
+3. Adjustable grid size, no larger than 100x100.
+4. Option to use random color per pixel.
--- script.js
+++ script.js
@@ -1,45 +1,82 @@
 document.addEventListener("DOMContentLoaded", function () {
     const sketchContainer = document.getElementById("container");
     const resetButton = document.getElementById("reset-button");
-    //generate the initial grid
+    const colorPicker = document.getElementById("color-picker");
+    const randomColorButton = document.getElementById("random-color-button");
+
+    let interactions = 0; // Counter for tracking interactions
+    let gridSize = 16; // Initial grid size
+    let selectedColor = "random"; // Default color
+    let isSketching = false; // To indicate is sketch is active
+ 
+    // Generate the initial grid
+    createGrid(gridSize);
     function createGrid(size) {
         sketchContainer.innerHTML = "";
-        sketchContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-        sketchContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+        sketchContainer.style.setProperty("--grid-size", size);
         for (let i = 0; i < size * size; i++) {
             const square = document.createElement("div");
             square.classList.add("square");
-            let interactions = 0;
-            //add event listener for hover effect
-            square.addEventListener("mouseover", function () {
-                interactions++;
-                const randomColor = getRandomRGB();
-                this.style.backgroundColor = randomColor;
-                const darkenedColor = darkenedColor(randomColor, interactions);
-                this.style.backgroundColor = darkenedColor;
-            });
+            // Add event listener for hover effect
+            square.addEventListener("mousedown", startSketching);
+            square.addEventListener("mouseup", stopSketching);
+            square.addEventListener("mouseover", handleSquareHover);
             sketchContainer.appendChild(square);
         }
     }
-    createGrid(16); //initial grid size
-    //reset button event listener
+
+    function startSketching() {
+        isSketching = true;
+    }
+
+    function stopSketching() {
+        isSketching = false;
+    }
+
+    function handleSquareHover() {
+        if (isSketching) {
+            interactions++;
+            let color;
+            if (selectedColor === "random") {
+                color = getRandomRGB(); // Generate a random color
+            } else {
+                color = selectedColor; // Use the selected color from the color picker
+            }
+            this.style.backgroundColor = color;
+            const darkenedColor = darkenColor(color, interactions);
+            this.style.backgroundColor = darkenedColor;
+        }
+    }
+
+    // Reset button event listener
     resetButton.addEventListener("click", function () {
         const newSize = prompt("Enter a new grid size (1-100):");
         if (newSize !== null && newSize >= 1 && newSize <= 100) {
-            createGrid(newSize);//convert string input into number
+            gridSize = parseInt(newSize);
+            createGrid(newSize); // Reset the grid with the new size
         } else {
             alert("Invalid grid size!");
         }
     });
-    //generate a random rgb color
+
+    // Color picker event listener
+    colorPicker.addEventListener("input", function () {
+        selectedColor = colorPicker.value; // Updated the color when it changes
+    });
+
+    // Generate a random rgb color
+    randomColorButton.addEventListener("click", function () {
+        selectedColor = "random"; // Set selected color to random
+    });
     function getRandomRGB() {
         const r = Math.floor(Math.random() * 256);
         const g = Math.floor(Math.random() * 256);
         const b = Math.floor(Math.random() * 256);
         return `rgb(${r}, ${g}, ${b})`;
     }
-    //darken color based on the number of interactions
-   function darkenedColor(color, interactions) {
+
+    // Darken color based on the number of interactions
+   function darkenColor(color, interactions) {
         const percent = interactions * 10;
         const darkenedColor = color.replace("rgb", "rgba").replace(")", `,${percent / 100}`);
         return darkenedColor;
